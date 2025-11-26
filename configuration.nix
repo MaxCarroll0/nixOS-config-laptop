b9d4152e1e101011b721ede87e5b1362ab2dcbe2
Max <mjvcarroll@gmail.com>
2025-11-26 18:25:08 +0000

    Grub

--- configuration.nix
+++ configuration.nix
@@ -11,8 +11,10 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
