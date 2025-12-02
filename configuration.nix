06984bc30cf56984408bfb6a2a3e3c3e35301b70
Max <mjvcarroll@gmail.com>
2025-12-02 15:03:21 +0000

    Fix efi system mount point

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,7 @@
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.extraEntries = ''
