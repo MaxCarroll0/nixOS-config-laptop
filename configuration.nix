b7e4f383a2c53f4fd6ff7ea5fc2ba7ef045bc17a
Max <mjvcarroll@gmail.com>
2025-12-02 14:43:15 +0000

    audio update

--- configuration.nix
+++ configuration.nix
@@ -86,13 +86,13 @@
   services.logind.lidSwitch = "ignore";
 
   # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
+  # services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
+    # pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
